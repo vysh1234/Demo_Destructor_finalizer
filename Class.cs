b09a70b7b2ba1ae09c01d722c58f44ca3c2524b9
vysh1234 <75359385+vysh1234@users.noreply.github.com>
2021-10-18 16:17:32 +0530

    destructor_finalizer

---
+++ Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Destructor
+{
+    public class Class : IDisposable
+    {
+        public void Display_Msg()
+        {
+            Console.WriteLine("inside display method()");
+        }
+        public Class()
+        {
+            Console.WriteLine("Default constructor..");
+        }
+        public Class(string msg)
+        {
+            Console.WriteLine("parametrized constructor..");
+        }
+        ~Class()
+        {
+            Console.WriteLine("Freeing up resources");
+        }
+        //public override string ToString() => GetType().Name;
+        
+        public void Dispose()
+        {
+            Console.WriteLine("Freeing up resources with dispose.");
+           
+            GC.SuppressFinalize(this);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo_Destructor
+{
+    class Program : Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo finalizer!");
+            Class obj1 = new Class();
+            obj1.Display_Msg();
+
+            //Class obj2 = new Class("Secret message");
+            //obj2.Display_Msg();
+
+            obj1.Dispose();
+
+            
+        }
+    }
+}
